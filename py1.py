2162e8d3ad30640ec6afee493f21ac547897e3be
madhabpaul <madhab12paul@gmail.com>
2019-02-11 22:53:07 +0530

    Pyhton Choice Program

--- py1.py
+++ py1.py
@@ -1,11 +1,20 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 import os
-var=int(input("Enter 1 for date, 2 for calender, 3 for poweroff, 4 for "))
+import sys
+import webbrowser
+var=int(input("Enter 1 for date, 2 for calender, 3 for poweroff, 4 for IP, 5 for All ip, 6 for google search"))
 if(var==1):
 	os.system('date')
 elif(var==2):
 	os.system('cal')
 elif(var==3):
-	os.system('poweroff')
+	os.system('shutdown -H -T -30')
+elif(var==4):
+	os.system('hostname -I')
+elif(var==5):
+	os.system('ifconfig')
+elif(var==6):
+	se = input("Enter Keywords ")
+	webbrowser.open_new_tab('https://www.google.com/search?q={}.format(se)')
 else:
 	print("Nothing Found")
